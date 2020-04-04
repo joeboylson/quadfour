33e6f2de36d34ca1bb99f907c31ef788cd3dd275
Joe Boylson <joeboylson@gmail.com>
2020-04-04 14:01:50 -0400

    WIP: working out drag and drop

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -10,62 +10,68 @@ const openModal = () => {
 
 // DRAG AND DROP
 
-const updateTask = async (taskId, isHighImportance, isHighUrgency) => {
+const updateTask = async (taskId, taskText, isHighImportance, isHighUrgency) => {
 
     const postUrl = `/save?taskId=${taskId}`;
-
     const formData = new FormData();
 
     formData.append('taskId', taskId);
     formData.append('isHighImportance', isHighImportance);
     formData.append('isHighUrgency', isHighUrgency);
-    formData.append('taskText', 'drag-and-drop');
+    formData.append('taskText', taskText);
 
-    console.log('FORM DATA', formData)
-
-    const config = {
-        headers: {'content-type': 'multipart/form-data'}
-    };
-
+    const config = { headers: {'content-type': 'multipart/form-data'} };
     return axios.post(postUrl, formData, config)
     .then( response => {
         return true;
-    })
-    .catch( error => {
+    }).catch( error => {
         return false;
     });
 
 };
 
+const removeAllDraggingOverClass = () => {
+    let elements = document.getElementsByClassName('dragging-over');
+    for (let element of elements) {
+        element.classList.remove('dragging-over');
+    }
+}
+
 const dragStart = (event) => {
 
     // dataset attributes are converted to lowercase (taskId ==> taskid)
     let taskId = event.target.dataset.taskid
+    let taskText = event.target.dataset.tasktext
 
     event.dataTransfer.setData("taskId", taskId);
+    event.dataTransfer.setData("taskText", taskText);
+
 }
 
 const dragEnd = (event) => {
-    console.log("DRAG END")
+    removeAllDraggingOverClass();
 }
 
 const allowDrop = (event) => {
     event.preventDefault();
-    console.log("DRAGGING . . .")
+    const hasDraggingOverClass = event.target.classList.contains('dragging-over');
+
+    if (!hasDraggingOverClass) {
+        removeAllDraggingOverClass();
+        event.target.classList.add('dragging-over')
+    }
 }
 
 const drop = (event) => {
     event.preventDefault();
-    console.log("TASK ID:", )
 
-    let taskId = event.dataTransfer.getData("taskId")
+    let taskId = event.dataTransfer.getData("taskId");
+    let taskText = event.dataTransfer.getData("taskText");
     let newIsHighImportance = event.target.dataset.ishighimportance === "true";
     let newIsHighUrgency = event.target.dataset.ishighurgency === "true";
 
-    updateTask(taskId, newIsHighImportance, newIsHighUrgency)
+    updateTask(taskId, taskText, newIsHighImportance, newIsHighUrgency)
     .then( success => {
-
-        console.log('UPDATE SUCCESSFUL?', success)
 
         if (success) {
             let taskElement = document.getElementById(`task-${taskId}`);
@@ -74,7 +80,5 @@ const drop = (event) => {
             event.target.appendChild(taskElement);
             return;
         }
-
-        console.log('UPDATE FAILED')
     })
 }
